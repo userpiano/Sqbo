fd9197ab10c7b23369d861da90783d4964a2fce3
userpiano <neon_212@mail.ru>
2020-05-22 17:44:36 +0300

    в

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
-const app = require('express')()
+const app = require('express')();
 const listener = app.listen(process.env.PORT, () => console.log('Уже подключились к порту: ' + listener.address().port))
